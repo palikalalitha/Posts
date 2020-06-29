220de99b1de3ab2169de7e833286ad82b8dfe839
Sunny <sanyasirao@ibhubs.co>
2020-06-29 19:11:42 +0530

    Add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'mobx-react'
+import { I18nextProvider } from 'react-i18next'
 
 import HomeRoute from './routes/HomeRoute'
 import SampleRoute from './routes/SampleRoute'
@@ -12,24 +13,31 @@ import {
   SAMPLE_ROUTE_PATH,
   TODOS_ROUTE_PATH
 } from './constants/NavigationConstants'
-import TodosRoute from './routes/TodosRoute'
+
+const TodosRoute = lazy(() => import('./routes/TodosRoute'))
+
+import i18n from './i18n'
 
 const App = () => {
   return (
     <Provider {...stores}>
-      <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route exact path={SAMPLE_ROUTE_PATH}>
-            <SampleRoute />
-          </Route>
-          <Route exact path={TODOS_ROUTE_PATH}>
-            <TodosRoute />
-          </Route>
-          <Route path={HOME_ROUTE_PATH}>
-            <HomeRoute />
-          </Route>
-        </Switch>
-      </Router>
+      <I18nextProvider i18n={i18n}>
+        <Suspense fallback={<div />}>
+          <Router basename={process.env.PUBLIC_URL}>
+            <Switch>
+              <Route exact path={SAMPLE_ROUTE_PATH}>
+                <SampleRoute />
+              </Route>
+              <Route exact path={TODOS_ROUTE_PATH}>
+                <TodosRoute />
+              </Route>
+              <Route path={HOME_ROUTE_PATH}>
+                <HomeRoute />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
+      </I18nextProvider>
     </Provider>
   )
 }
--- src/routes/SampleRoute/index.tsx
+++ src/routes/SampleRoute/index.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function SampleRoute() {
-  return <h1>SampleRoute</h1>
+import { withTranslation, WithTranslation } from 'react-i18next'
+
+import {
+  StringsContainer,
+  DemoString,
+  DemoStringsSectionTitle
+} from './styledComponents'
+
+interface SampleRouteProps extends WithTranslation {}
+
+class SampleRoute extends Component<SampleRouteProps> {
+  render() {
+    const { t } = this.props
+    return (
+      <StringsContainer>
+        <DemoStringsSectionTitle>
+          {t('todos:useCases.normal')}
+        </DemoStringsSectionTitle>
+        <DemoString>{t('common:failureScreen.retry')}</DemoString>
+        <DemoString>{t('todos:addTodo')}</DemoString>
+        <DemoStringsSectionTitle>
+          {t('todos:useCases.plural')}
+        </DemoStringsSectionTitle>
+        <DemoString>{t('todos:todosLeft', { count: 1 })}</DemoString>
+        <DemoString>{t('todos:todosLeft', { count: 2 })}</DemoString>
+        <DemoStringsSectionTitle>
+          {t('todos:useCases.parametrized')}
+        </DemoStringsSectionTitle>
+        <DemoString>{t('todos:todosLeftParameter', { count: 1 })}</DemoString>
+        <DemoString>{t('todos:todosLeftParameter', { count: 2 })}</DemoString>
+        <DemoString>
+          {t('todos:todoWithParameter', { todoText: 'Learning i18n' })}
+        </DemoString>
+      </StringsContainer>
+    )
+  }
 }
-
-export default SampleRoute
+export default withTranslation('translation', { withRef: true })(SampleRoute)
